2bf013df62ec517578c73bd1a06cb0d9317347f5
bright4862-design <bright4862@gmail.com>
2026-07-07 02:58:34 +0200

    Support Base44 API key environment parameter

--- src/lib/app-params.js
+++ src/lib/app-params.js
@@ -42,6 +42,7 @@ const getAppParams = () => {
 	return {
 		appId: getAppParamValue("app_id", { defaultValue: import.meta.env.VITE_BASE44_APP_ID }),
 		token: getAppParamValue("access_token", { removeFromUrl: true }),
+		apiKey: getAppParamValue("api_key", { defaultValue: import.meta.env.VITE_BASE44_API_KEY, removeFromUrl: true }),
 		fromUrl: getAppParamValue("from_url", { defaultValue: window.location.href }),
 		functionsVersion: getAppParamValue("functions_version", { defaultValue: import.meta.env.VITE_BASE44_FUNCTIONS_VERSION }),
 		appBaseUrl: getAppParamValue("app_base_url", { defaultValue: import.meta.env.VITE_BASE44_APP_BASE_URL }),

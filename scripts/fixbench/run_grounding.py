5ab14a19e799628e4986b660b49d91b9e238b11a
William Bright <bright4862@gmail.com>
2026-09-23 07:10:39 +0200

    test(ai): register root-contract FixBench corpus

--- scripts/fixbench/run_grounding.py
+++ scripts/fixbench/run_grounding.py
@@ -19,6 +19,7 @@ _SUPPORTED_FIXTURES = frozenset({
     "fixbench_grounding_url_scope_v1",
     "fixbench_grounding_nested_scope_v1",
     "fixbench_grounding_identity_scope_v1",
+    "fixbench_grounding_root_contract_v1",
 })
 
 
